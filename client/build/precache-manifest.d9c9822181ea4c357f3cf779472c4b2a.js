177a04448342909ee6f3ce7d5484304a88a19241
stephen England <stephenengland@stephens-MacBook-Pro.local>
2020-03-12 19:35:31 -0400

    new

---
+++ client/build/precache-manifest.d9c9822181ea4c357f3cf779472c4b2a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "543fbda09e78bdd90a18",
+    "url": "/static/css/main.9b7d5566.chunk.css"
+  },
+  {
+    "revision": "543fbda09e78bdd90a18",
+    "url": "/static/js/main.543fbda0.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "1e6a615f1af8224ee70f",
+    "url": "/static/js/2.1e6a615f.chunk.js"
+  },
+  {
+    "revision": "de6a7e7dae3812e4e283d076442a4583",
+    "url": "/index.html"
+  }
+];
